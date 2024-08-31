b503e41a723080d3433c81609e9b8fcbfc2ce496
Houlaymatou B <houlaymatoubalde@HoulaymatousMBP.lan>
2024-08-31 10:13:49 -0500

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "your Car Buddy",
+  description:
+    "Car Buddy simplifies your car maintenance with smart reminders and insightful advice. Say goodbye to confusion and overpaying—experience effortless car care with just a few taps.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
