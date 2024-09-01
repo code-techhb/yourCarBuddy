a170482585de2ead7db480536d748bffb8e06819
Houlaymatou B <houlaymatoubalde@HoulaymatousMBP.lan>
2024-08-31 20:48:42 -0500

    set up theme and images

---
+++ app/components/theme.js
@@ -0,0 +1,50 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#003632",
+      secondary: "#00D779",
+      light: "#D1DFBA",
+      white: "#FFFFFF",
+      black: "#000000",
+      chatbox: "#F5F1EF",
+    },
+  },
+
+  custom: {
+    background:
+      "linear-gradient(359deg, rgba(0, 0, 0, 0.00) 37.02%, rgba(0, 0, 0, 0.65) 103.1%), #005852",
+    thin_background: "rgba(26, 26, 26, 0.80)",
+    hero_background_gradient:
+      "linear-gradient(356deg, rgba(217, 217, 217, 0.00) -56.4%, #003632 49.74%)",
+  },
+
+  typography: {
+    fontFamily: "Anton, Montserrat, Arial, sans-serif",
+    h1: {
+      fontFamily: "Anton",
+    },
+    h2: {
+      fontFamily: "Anton",
+    },
+    h3: {
+      fontFamily: "Anton",
+    },
+    h4: {
+      fontFamily: "Anton",
+    },
+    h5: {
+      fontFamily: "Anton",
+    },
+    h6: {
+      fontFamily: "Anton",
+    },
+    body1: {
+      fontFamily: "Montserrat",
+      fontSize: "24px",
+    },
+  },
+});
+
+export default theme;
