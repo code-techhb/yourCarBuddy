96c8e5a41b4422aabe52f7e97692a63457a29437
Houlaymatou B <houlaymatoubalde@HoulaymatousMBP.lan>
2024-09-01 21:39:19 -0500

    layout

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
