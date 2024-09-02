fda0c13ca446d68c9c435ff80331ac747dcfa02a
Houlaymatou B <houlaymatoubalde@HoulaymatousMBP.lan>
2024-09-01 21:44:52 -0500

    corrected clerk import

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-//import styles from "./page.module.css";
+
 import { ThemeProvider, Box, CssBaseline } from "@mui/material";
 import Hero from "./components/hero";
 import Head from "next/head";
@@ -10,82 +10,78 @@ import Theme from "./components/theme";
 import MiddleHero from "./components/hero2";
 import FeaturesAI from "./components/features";
 import Footer from "./components/footer";
-import Profile from "./profile/page";
-import { ClerkProvider } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <ClerkProvider>
-      <ThemeProvider theme={Theme}>
-        <CssBaseline />
-        <Head>
-          <title>Car Buddy</title>
-          <meta name="description" content="Car Buddy"></meta>
-        </Head>
+    <ThemeProvider theme={Theme}>
+      <CssBaseline />
+      <Head>
+        <title>Car Buddy</title>
+        <meta name="description" content="Car Buddy"></meta>
+      </Head>
 
-        <Box
-          sx={{
-            minHeight: "100vh",
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
+      <Box
+        sx={{
+          minHeight: "100vh",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
 
-            height: "100vh",
-            backgroundImage: `${Theme.custom.hero_background_gradient}, 
+          height: "100vh",
+          backgroundImage: `${Theme.custom.hero_background_gradient}, 
                               url('car.png')`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Navbar />
-          <Box
-            sx={{
-              flex: 1,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Hero></Hero>
-          </Box>
-        </Box>
-
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Navbar />
         <Box
           sx={{
+            flex: 1,
             display: "flex",
             flexDirection: "column",
-            background: `${Theme.custom.hero_background_gradient}`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            display: "flex",
-            justifyContent: "center",
             alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          <MiddleHero />
+          <Hero></Hero>
         </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            background: "#D1DFBA",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-          }}
-        >
-          <FeaturesAI />
-        </Box>
+      </Box>
 
-        <Footer></Footer>
-      </ThemeProvider>
-    </ClerkProvider>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          background: `${Theme.custom.hero_background_gradient}`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <MiddleHero />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          background: "#D1DFBA",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <FeaturesAI />
+      </Box>
+
+      <Footer></Footer>
+    </ThemeProvider>
   );
 }
